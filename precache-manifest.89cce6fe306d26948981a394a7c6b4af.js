720f64f58700ab9e41d4f234b6c4e6ba196facc7
Thunderarea <dai16069@uom.edu.gr>
2020-06-16 10:44:19 +0300

    Updates

---
+++ precache-manifest.89cce6fe306d26948981a394a7c6b4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02be6a4865a0996d7b2ac2fe95aba22",
+    "url": "/css-editor/index.html"
+  },
+  {
+    "revision": "9ee62dcc35034c92e26b",
+    "url": "/css-editor/static/css/main.90a7ce3b.chunk.css"
+  },
+  {
+    "revision": "44766ed2425eaf8845b0",
+    "url": "/css-editor/static/js/2.68b4ccea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/css-editor/static/js/2.68b4ccea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee62dcc35034c92e26b",
+    "url": "/css-editor/static/js/main.7029a6b9.chunk.js"
+  },
+  {
+    "revision": "97bbf7fd5b0f765f894c",
+    "url": "/css-editor/static/js/runtime-main.7e4f0ed1.js"
+  }
+]);
